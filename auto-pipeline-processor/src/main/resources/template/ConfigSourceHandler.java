80a5adc9c711915b48286fffacebe74d88696b7e
zavakid <zava.kid@gmail.com>
2021-12-18 23:22:25 +0800

    = code cleanup

--- auto-pipeline-processor/src/main/resources/template/ConfigSourceHandler.java
+++
@@ -1,7 +0,0 @@
-package com.foldright.example.config;
-
-public interface ConfigSourceHandler {
-
-    String get(String key, ConfigSourceHandlerContext context);
-
-}
--- auto-pipeline-processor/src/main/resources/template/ConfigSourceHandlerContext.java
+++
@@ -1,6 +0,0 @@
-package com.foldright.example.config;
-
-public interface ConfigSourceHandlerContext extends ConfigSource {
-
-    ConfigSourcePipeline pipeline();
-}
