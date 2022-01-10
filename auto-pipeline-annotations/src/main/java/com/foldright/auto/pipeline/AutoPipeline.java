79993bc7dbbd1545d9989494e3a88f4354d5b7d1
zavakid <zava.kid@gmail.com>
2022-01-10 21:00:35 +0800

    docs: add docs to AutoPipeline

--- auto-pipeline-annotations/src/main/java/com/foldright/auto/pipeline/AutoPipeline.java
+++ auto-pipeline-annotations/src/main/java/com/foldright/auto/pipeline/AutoPipeline.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The annotation to be applied to an interface for which the pipeline pattern should be automatically generated.
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.CLASS)
 public @interface AutoPipeline {
