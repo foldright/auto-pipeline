8a00222f60025460ffdf05c4a4554ec6292e9cc2
Jerry Lee <oldratlee@gmail.com>
2022-02-04 23:34:00 +0800

    test: test `AutoPipelineProcessor` using lib `kotlin-compile-testing`

--- auto-pipeline-processor/src/test/java/com/foldright/auto/pipeline/AutoPipelineProcessorTest.kt
+++ auto-pipeline-processor/src/test/java/com/foldright/auto/pipeline/AutoPipelineProcessorTest.kt
@@ -1,22 +1,42 @@
 package com.foldright.auto.pipeline
 
 import com.foldright.auto.pipeline.processor.AutoPipelineProcessor
-import com.google.testing.compile.Compiler
-import com.google.testing.compile.JavaFileObjects
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
 import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import org.jetbrains.kotlin.konan.file.File
 
+/**
+ * [`kotlin-compile-testing` usage](https://github.com/tschuchortdev/kotlin-compile-testing)
+ */
 class AutoPipelineProcessorTest : AnnotationSpec() {
-
     @Test
-    @Ignore
-    fun test() {
-        val configInterface = JavaFileObjects.forResource("test1/ConfigSource.java")
+    fun test_AutoPipelineProcessor() {
+        val file = "test1/ConfigSource.java"
 
-        val compilation = Compiler.javac()
-            .withProcessors(AutoPipelineProcessor())
-            .compile(configInterface)
+        val compileResult = KotlinCompilation().apply {
+            sources = listOf(SourceFile.java(file.fileName(), file.contentFromClassPath()))
+            annotationProcessors = listOf(AutoPipelineProcessor())
 
-        compilation.generatedSourceFiles().forEach { println(it.getCharContent(true)) }
+            inheritClassPath = true
 
+            // see compile diagnostics in real time?
+            verbose = false
+        }.compile()
+
+        compileResult.exitCode shouldBe KotlinCompilation.ExitCode.OK
+
+        val sourcesGenerated = compileResult.sourcesGeneratedByAnnotationProcessor
+        sourcesGenerated shouldHaveSize 5
+        sourcesGenerated.forEach {
+            println("\ncontent of generated ${it.name}(${it.canonicalPath}):\n${it.readText()}")
+        }
     }
+
+    private fun String.fileName() = File(this).name
+
+    private fun String.contentFromClassPath() =
+        AutoPipelineProcessorTest::class.java.classLoader.getResource(this)!!.readText()
 }
