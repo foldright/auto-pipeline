e6324e783d17de8098d1608480e1f43961a4191f
zavakid <zava.kid@gmail.com>
2025-12-28 21:41:27 +0800

    feat: use kotlin 2.3.0 and remove kctfork

--- auto-pipeline-processor/src/test/java/com/foldright/auto/pipeline/AutoPipelineProcessorTest.kt
+++ auto-pipeline-processor/src/test/java/com/foldright/auto/pipeline/AutoPipelineProcessorTest.kt
@@ -1,100 +1,66 @@
 package com.foldright.auto.pipeline
 
 import com.foldright.auto.pipeline.processor.AutoPipelineProcessor
-import com.tschuchort.compiletesting.KotlinCompilation
-import com.tschuchort.compiletesting.SourceFile
+import com.google.testing.compile.Compilation
+import com.google.testing.compile.Compiler
+import com.google.testing.compile.JavaFileObjects
 import io.kotest.core.spec.style.AnnotationSpec
-import io.kotest.engine.spec.tempdir
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
-import java.io.File
+import javax.tools.JavaFileObject.Kind
 
 /**
- * [`kotlin-compile-testing` usage](https://github.com/ZacSweers/kotlin-compile-testing)
+ * [`compile-testing` usage](https://github.com/google/compile-testing)
  */
 class AutoPipelineProcessorTest : AnnotationSpec() {
-    @OptIn(ExperimentalCompilerApi::class)
     @Test
     fun test_AutoPipelineProcessor() {
-        val compileResult = KotlinCompilation().apply {
-            sources = listOf(javaSourceFileOnClassPath("test1/ConfigSource.java"))
-            annotationProcessors = listOf(AutoPipelineProcessor())
+        val compiled = Compiler.javac().withProcessors(AutoPipelineProcessor())
+            .compile(JavaFileObjects.forResource("test1/ConfigSource.java"))
 
-            inheritClassPath = true
+        compiled.errors().size shouldBe 0
+        compiled.warnings().size shouldBe 0
 
-            // see compile diagnostics in real time?
-            verbose = false
-
-            // set work dir via `io.kotest.engine.spec.TempdirKt.tempdir`
-            // so auto delete the temp dir after test
-            workingDir = tempdir(AutoPipelineProcessorTest::class.qualifiedName)
-        }.compile()
-
-        compileResult.exitCode shouldBe KotlinCompilation.ExitCode.OK
-
-        val sourcesGenerated = compileResult.sourcesGeneratedByAnnotationProcessor
-        sourcesGenerated shouldHaveSize 5
-        sourcesGenerated.forEach {
-            println("\ncontent of generated ${it.name}(${it.canonicalPath}):\n${it.readText()}")
-        }
+        compiled.status() shouldBe Compilation.Status.SUCCESS
+        compiled.generatedFiles().filter { it.kind == Kind.SOURCE } shouldHaveSize 5
+        compiled.generatedFiles()
+            .filter { it.kind == Kind.SOURCE }
+            .forEach {
+                println("\ncontent of generated ${it.name}:\n${it.getCharContent(false)}")
+            }
     }
 
-    @OptIn(ExperimentalCompilerApi::class)
     @Test
     fun test_AutoPipelineProcessor_Duplex() {
-        val compileResult = KotlinCompilation().apply {
-            sources = listOf(javaSourceFileOnClassPath("test1/RPC.java"))
-            annotationProcessors = listOf(AutoPipelineProcessor())
 
-            inheritClassPath = true
+        val compiled = Compiler.javac().withProcessors(AutoPipelineProcessor())
+            .compile(JavaFileObjects.forResource("test1/RPC.java"))
 
-            // see compile diagnostics in real time?
-            verbose = false
+        compiled.errors().size shouldBe 0
+        compiled.warnings().size shouldBe 0
 
-            // set work dir via `io.kotest.engine.spec.TempdirKt.tempdir`
-            // so auto delete the temp dir after test
-            workingDir = tempdir(AutoPipelineProcessorTest::class.qualifiedName)
-        }.compile()
-
-        compileResult.exitCode shouldBe KotlinCompilation.ExitCode.OK
-
-        val sourcesGenerated = compileResult.sourcesGeneratedByAnnotationProcessor
-        sourcesGenerated shouldHaveSize 5
-        sourcesGenerated.forEach {
-            println("\ncontent of generated ${it.name}(${it.canonicalPath}):\n${it.readText()}")
-        }
+        compiled.status() shouldBe Compilation.Status.SUCCESS
+        compiled.generatedFiles().filter { it.kind == Kind.SOURCE } shouldHaveSize 5
+        compiled.generatedFiles()
+            .filter { it.kind == Kind.SOURCE }
+            .forEach {
+                println("\ncontent of generated ${it.name}:\n${it.getCharContent(false)}")
+            }
     }
 
-    @OptIn(ExperimentalCompilerApi::class)
     @Test
     fun test_AutoPipelineProcessor_Generic() {
-        val compileResult = KotlinCompilation().apply {
-            sources = listOf(javaSourceFileOnClassPath("test1/Channel.java"))
-            annotationProcessors = listOf(AutoPipelineProcessor())
+        val compiled = Compiler.javac().withProcessors(AutoPipelineProcessor())
+            .compile(JavaFileObjects.forResource("test1/Channel.java"))
 
-            inheritClassPath = true
+        compiled.errors().size shouldBe 0
 
-            // see compile diagnostics in real time?
-            verbose = false
-
-            // set work dir via `io.kotest.engine.spec.TempdirKt.tempdir`
-            // so auto delete the temp dir after test
-            workingDir = tempdir(AutoPipelineProcessorTest::class.qualifiedName)
-        }.compile()
-
-        compileResult.exitCode shouldBe KotlinCompilation.ExitCode.OK
-
-        val sourcesGenerated = compileResult.sourcesGeneratedByAnnotationProcessor
-        sourcesGenerated shouldHaveSize 5
-        sourcesGenerated.forEach {
-            println("\ncontent of generated ${it.name}(${it.canonicalPath}):\n${it.readText()}")
-        }
+        compiled.status() shouldBe Compilation.Status.SUCCESS
+        compiled.generatedFiles().filter { it.kind == Kind.SOURCE } shouldHaveSize 5
+        compiled.generatedFiles()
+            .filter { it.kind == Kind.SOURCE }
+            .forEach {
+                println("\ncontent of generated ${it.name}:\n${it.getCharContent(false)}")
+            }
     }
-
-    @Suppress("SameParameterValue")
-    private fun javaSourceFileOnClassPath(filePath: String): SourceFile = SourceFile.java(
-        File(filePath).name,
-        javaClass.classLoader.getResource(filePath)!!.readText()
-    )
 }
